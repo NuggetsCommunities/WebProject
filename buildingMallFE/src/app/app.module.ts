83755ba79e052187405267137775dca111d6279c
IT_Ann <an_ning914@163.com>
2017-11-18 01:16:19 +0800

    update register fuc ，add router to  register

--- buildingMallFE/src/app/app.module.ts
+++ buildingMallFE/src/app/app.module.ts
@@ -12,7 +12,16 @@ import { CarouselComponent } from './main/carousel/carousel.component';
 import { RecommandBrandComponent } from './main/recommand-brand/recommand-brand.component';
 import { IndexComponent } from './user/index/index.component';
 import { RegisterComponent } from './user/register/register.component';
-
+  // 表单 双向数据绑定
+  import {FormsModule} from "@angular/forms";
+import { Route, Routes } from '@angular/router/src/config';
+//定义常量 嵌套路由
+const logonChildRoutes:Routes = [
+  {path:"singIn", component:LogonComponent},
+  {path:"register",component:RegisterComponent},
+  //默认跳转到登录页面,这种写法 '**'=>相当于path：'',并且pathMath:'full'
+  {path:"**",redirectTo:"singIn"}
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -41,7 +50,8 @@ import { RegisterComponent } from './user/register/register.component';
       },
       {
         path:"logon",
-        component:IndexComponent
+        component:IndexComponent,
+        children:logonChildRoutes
       }
      
      
